e7b71efd0b02fc3fc5390d36438bdf8736f8df7c
Kadiri victor <kadiriavictor@gmail.com>
2022-06-02 21:47:15 +0000

    commit x

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - The main function
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+	int y;
+	long int z;
+	long long int a;
+	float b;
+	printf("Size of a char: %ld byte (s)\n", sizeof(x));
+	printf("Size of an int: %ld byte (s)\n", sizeof(y));
+	printf("Size of a long int: %ld byte (s)\n", sizeof(z));
+	printf("Size of a long long int: %ld byte (s)\n", sizeof(a));
+	printf("Size of a float: %ld byte (s)\n", sizeof(b));
+	return(0);
+}
---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("The size of int is: %ld\n", sizeof(int));
+	return (0);
+}
