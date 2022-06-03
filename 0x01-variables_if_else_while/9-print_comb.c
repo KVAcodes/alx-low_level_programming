a09da3f3e16a8f0d838e58e022468fe65226293a
Kadiri victor <kadiriavictor@gmail.com>
2022-06-03 19:12:46 +0000

    commit x

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,5 +7,19 @@
  */
 int main(void)
 {
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
 
 }
