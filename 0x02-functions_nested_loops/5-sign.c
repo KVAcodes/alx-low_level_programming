1a703ea9040205265c0cd135c9bad86add578046
Kadiri victor <kadiriavictor@gmail.com>
2022-06-07 21:19:06 +0000

    commit x

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: n is of type int
+ * Return: 1, 0 and -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
