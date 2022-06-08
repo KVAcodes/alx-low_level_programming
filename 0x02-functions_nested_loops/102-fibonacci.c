0dc132894e58c57ed457262c56206fececf8e5c3
Kadiri victor <kadiriavictor@gmail.com>
2022-06-08 04:49:40 +0000

    commit x

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print the first 50 fibonacci numbers.
+ * Return: void
+ */
+int main(void)
+{
+	unsigned long x, a, b, c;
+
+	a = 0;
+	b = 1;
+	for (x = 0; y < 50; z++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%lu", c);
+		if (x == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
+
+
