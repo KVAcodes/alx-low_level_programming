b8b29437c2fba8a68063808f93b5ebff8619d0e6
Kadiri victor <kadiriavictor@gmail.com>
2022-06-08 03:36:22 +0000

    commit x

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar ('+');
 		return (1);
-		_putchar('+');
 	}
 	else if (n == 0)
 	{
+		_putchar ('0');
 		return (0);
-		_putchar('0');
-	}
-	else if (n < 0)
-	{
-		return (-1);
-		_putchar('-');
 	}
 	else
-		return (3);
+	{
+		_putchar ('-');
+		return (-1);
+	}
 }
