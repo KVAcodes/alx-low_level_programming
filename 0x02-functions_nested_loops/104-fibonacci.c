fdeeff48ffa484775ab80e0dfc118d4d830efefb
Kadiri victor <kadiriavictor@gmail.com>
2022-06-08 22:56:47 +0000

    commit x

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,43 +5,20 @@
  */
 int main(void)
 {
-	int count;
-	unsigned long i, j, k;
-	unsigned long m, n, p, carry;
+	int i = 0;
+	unsigned long int a = 0, b = 1, next = 0;
 
-	count = 0;
-	i = 0;
-	j = 1;
-	for (count = 1; count <= 91; count++)
+	while (i < 98)
 	{
-		k = i + j;
-		i = j;
-		j = k;
-		printf("%lu, ", k);
-		m = i % 1000;
-		i = i / 1000;
-		n = j % 1000;
-		j = j / 1000;
-		while (count <= 98)
-		{
-			carry = (m + n) / 1000;
-			p = (m + n) - carry * 1000;
-			k = (i + j) + carry;
-			m = n;
-			n = p;
-			i = j;
-			j = k;
-			if (p >= 100)
-				printf("%lu%lu", k, p);
-			else
-				printf("%lu0%lu", k, p);
-			if (count != 98)
-				printf(",");
-			count++;
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
 
-		}
-		putchar('\n');
-	
+		if (i < 97)
+			printf(", ");
+		i++;
 	}
+	putchar('\n');
 	return (0);
 }
