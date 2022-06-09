362806ba340c99c4483739517d4f40dc26228572
Kadiri victor <kadiriavictor@gmail.com>
2022-06-09 21:05:53 +0000

    commit x

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,17 @@
  * print_most_numbers - prints 0 to 9 except 2 and 4
  * Return: void
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int x = 48;
 
-	while (x < 58)
+	while (x <= 57)
 	{
-		if (x != 50 || x != 52)
+		if (x != 50 && x != 52)
+		{
 			_putchar(x);
+		}
+		x++;
 	}
 	_putchar('\n');
 }
