fb90780aae9b7dbda1ab0662c44b4bbbaeac100e
Kadiri victor <kadiriavictor@gmail.com>
2022-06-10 01:20:10 +0000

    commit x

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,24 +8,23 @@
  */
 int main(void)
 {
-	int main(void)
+	int i;
+
+	for (i = 1; i <= 100; i++)
 	{
-		int i;
-
-		for (i = 1; i <= 100; i++)
-		{
-			if ((i % 5) == 0 && (i % 3) == 0)
-				printf("FizzBuzz");
-			else if ((i % 5) == 0)
-				printf("Buzz");
-			else if ((i % 3) == 0)
-				printf("Fizz");
-			else
-				printf("%d", i);
-			printf("%s", i < 100 ? " " : "");
-		}
-		printf("\n");
-
-		return (0);
+		if ((i % 5) == 0 && (i % 3) == 0)
+			printf("FizzBuzz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
+		else if ((i % 3) == 0)
+			printf("Fizz");
+		else
+			printf("%d", i);
+		printf("%s", i < 100 ? " " : "");
 	}
+	printf("\n");
+	return (0);
 }
+
+
+
