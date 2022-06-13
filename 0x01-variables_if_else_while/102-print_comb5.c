3c61a513ac6abec7b667205347107ed98d520ed4
Kadiri victor <kadiriavictor@gmail.com>
2022-06-13 10:05:25 +0000

    commit x

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,7 +8,36 @@
 int main(void)
 {
 	int a, b, c, d;
-	for (a = 0; a < 10; a++)
-		for(b = 0; b < 10; b++)
-			for (c= 0; 
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			for (c = 48; c < 58; c++)
+			{
+				for (d = 48; d < 58; d++)
+				{
+					if (((c + d) > (a + b) && c >= a) || a < c)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+					if ((a + b + c + d == 227) && (a == 57))
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
