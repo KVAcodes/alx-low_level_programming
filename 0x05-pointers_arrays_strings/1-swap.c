a9f7c3404680e6d81246a889edd0b06387f510e3
Kadiri victor <kadiriavictor@gmail.com>
2022-06-13 19:47:32 +0000

    commit x

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,4 +4,14 @@
  * swap_int - swaps the values of two integers
  * @a: pointer to 1st integer
  * @b: pointer to 2nd integer
+ *
+ * Return: void
  */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
