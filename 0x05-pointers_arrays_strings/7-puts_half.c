eff7424d43c9467a31326a604e7068bbc7eaea96
Kadiri victor <kadiriavictor@gmail.com>
2022-06-14 22:10:51 +0000

    commit x

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,11 +15,11 @@ void puts_half(char *str)
 	x--;
 	if (x % 2 == 0)
 	{
-		y = x / 2;
+		y = (x / 2) + 1;
 	}
 	else
 	{
-		y = (x - 1) / 2;
+		y = (x / 2) + 1 / 2;
 	}
 	for (z = y; z <= x; z++)
 	{
