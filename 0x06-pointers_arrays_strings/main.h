97b8284c6003411d239f5d499a7d5f1dae57907c
Kadiri victor <kadiriavictor@gmail.com>
2022-06-16 22:21:08 +0000

    commit x

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,4 +14,5 @@ void print_number(int n);
 char *infiniteadd(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
+void print_line(char *c, int s, int l);
 #endif 
