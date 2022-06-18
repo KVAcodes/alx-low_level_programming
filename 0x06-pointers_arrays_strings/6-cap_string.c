5d2a5677958d656de6cd582fb88ab28546235756
Kadiri victor <kadiriavictor@gmail.com>
2022-06-18 15:55:57 +0000

    commit x

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,6 +13,10 @@ char *cap_string(char *str)
 			     '"', '(', ')', '{', '}'};
 	for (i = 0; str[i] != '\0'; i++)
 	{
+		if (i == 0 && (str[i] >= 97 && str[i] <= 122))
+		{
+			str[i] -= 32;
+		}
 		for (x = 0; x <= 12; x++)
 		{
 			if ((str[i] == testarray[x]) && (str[i + 1] >= 97 && str[i + 1] <= 122))
