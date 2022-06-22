623146f59f268fd016ca0a509f98d1064c7dd26b
Kadiri victor <kadiriavictor@gmail.com>
2022-06-22 19:20:21 +0000

    commit x

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	else
-	{
-		_puts_recursion(s + 1);
-	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
