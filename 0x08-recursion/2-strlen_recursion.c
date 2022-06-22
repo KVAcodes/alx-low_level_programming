1d83b61848a20598bc9f1702747f5c4a27565850
Kadiri victor <kadiriavictor@gmail.com>
2022-06-22 20:00:58 +0000

    commit x

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of the input string
+ *  @s: input string
+ * Return: The length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s == '\0')
+	{
+		return(0);
+	}
+	length = _strlen_recursion(s + 1);
+	return (length + 1);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,4 @@
 0 - A recursive function that prints a string, followed by a new line.
 1 - A recursive function that prints a string in reverse.
+2 - A recursive function that returns the length of a string.
+3 - A recursive function
