e1fcd423755ebe691e371f651074190bfbe55fce
Kadiri victor <kadiriavictor@gmail.com>
2022-06-22 20:03:57 +0000

    commit x

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,11 @@
  */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
+	int length;
 
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
 	length = _strlen_recursion(s + 1);
 	return (length + 1);
