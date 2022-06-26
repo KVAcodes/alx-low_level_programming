df9bfb42e5e2046cfac2b3a586374d7677a28583
Kadiri victor <kadiriavictor@gmail.com>
2022-06-26 17:23:25 +0000

    commit x

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#define UNUSED(argv) (void)(argv)
 /**
- * main - prints the no arguments passed into it 
+ * main - prints the no arguments passed into it
  * @argc: no of arguments
  * @argv: array of string arguments
  * Return: 0 always a success
  */
 int main(int argc, char *argv[])
 {
-	(void)argv;
+	UNUSED(argv);
 	printf("%d\n", argc);
 	return (0);
 }
