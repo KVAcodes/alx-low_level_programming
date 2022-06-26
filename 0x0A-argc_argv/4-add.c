03c1347d15e4140b6c3050084259418ebdc40a02
Kadiri victor <kadiriavictor@gmail.com>
2022-06-26 20:33:52 +0000

    commit x

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * IsaInteger - checks if a string is an integer in disguise
+ * @str: string
+ * Return: 0 if str is an integer and 1 if not
+ */
+int IsaInteger(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+/**
  * main - adds positive numbers
  * @argc: no of arguments
  * @argv: array of string arguments
@@ -11,7 +30,7 @@ int main(int argc, char *argv[])
 	int i, res;
 
 	res = 0;
-	if (argc <= 1)
+	if (argc == 1)
 	{
 		printf("%d\n", 0);
 	}
@@ -19,11 +38,11 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] >= '0' && *argv[i] <= '9')
+			if (IsaInteger(argv[i]) == 0)
 			{
 				res += atoi(argv[i]);
 			}
-			else
+			else if (IsaInteger(argv[i]) == 1)
 			{
 				printf("Error\n");
 				return (1);
