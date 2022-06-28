f7844accada01a92d4965c7ad8297d6603d5d3fa
Kadiri victor <kadiriavictor@gmail.com>
2022-06-28 19:56:49 +0000

    commit x

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: a pointer to the concatenated string on success and NULL on
+ * failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int a, b, x, y, z;
+	char *s;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	else if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (x = 0; s1[x] != 0; x++)
+	{
+	}
+	for (y = 0; s2[y] != 0; y++)
+	{
+	}
+	z = x + y + 1;
+
+	s = malloc(z * sizeof(char));
+
+	for (a = 0; s1[a] != 0; a++)
+	{
+		s[a] = s1[a];
+	}
+	for (b = 0; s2[b] != 0; b++)
+	{
+		s[a + b] = s2[b];
+	}
+	s[a + b] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,4 +1,4 @@
 Note that each task has underlying instructions not stated in this README
 0 - A function that creates an array of chars and initializes it with a specific char.
 1 - A function that returns a pointer to a newly allocated space in memory which contains a copy of the string given as a parameter.
-2 -
+2 - A function that concatenates two strings.
