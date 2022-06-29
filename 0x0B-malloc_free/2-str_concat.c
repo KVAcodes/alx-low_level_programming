be15b1773d1b6918eaddba0ff46c2e09577e1c85
Kadiri victor <kadiriavictor@gmail.com>
2022-06-29 15:27:28 +0000

    commit x

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,32 +10,39 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *strnew = NULL;
-	unsigned int i;
-	int n1;
-	int n2;
-	int count;
+	int a, b, x, y, z;
+	char *s;
+	b = 0;
 
-	count = 0;
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-	for (n1 = 0; s1[n1] != '\0'; n1++)
-		;
-	for (n2 = 0; s2[n2] != '\0'; n2++)
-		;
-	strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
-	if (strnew == NULL)
+	}
+	for (x = 0; s1[x] != 0; x++)
+	{
+	}
+	for (y = 0; s2[y] != 0; y++)
+	{
+	}
+	z = x + y;
+	s = malloc((z + 1) * sizeof(char));
+	if (s  == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i] != '\0'; i++)
-		strnew[i] = s1[i];
-	for (; s2[count] != '\0'; i++)
+	for (a = 0; s1[a] != 0; a++)
 	{
-		strnew[i] = s2[count];
-		count++;
+		s[a] = s1[a];
 	}
-	return (strnew);
+	for (; s2[b] != 0; a++)
+	{
+		s[a] = s2[b];
+		b++;
+	}
+	s[z] = '\0';
+	return (s);
 }
--- 0x0B-malloc_free/test.c
+++ 0x0B-malloc_free/test.c
@@ -2,14 +2,8 @@
 #include <stdlib.h>
 int main(void)
 {
-	int i;
-	char *str = NULL;
-	if (str[0] == '\0')
-	{
-		printf("yes\n");
-	}
-	else
-	{
-		printf("no\n");
-	}
+	char *str = "Holberton";
+	str = "";
+	printf("%s\n", str);
+	return (0);
 }
