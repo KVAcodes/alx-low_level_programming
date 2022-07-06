ed43f0c07c5938a4d85ec520eec19879a8200a3e
Kadiri victor <kadiriavictor@gmail.com>
2022-07-06 19:13:15 +0000

    commit x

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,8 @@
 /**
  * print_name - This prints a name
  * @name: The name to be printed
- * @f: a function pointer to a function that returns nothing and takes in a string
+ * @f: a function pointer to a function that returns nothing
+ * and takes in a string
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
