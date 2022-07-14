1240d4b73180e550c1bba6b87c22a99e52823208
Kadiri victor <kadiriavictor@gmail.com>
2022-07-14 21:28:32 +0000

    commit x

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+
+/**
+ * add_node - adds a new node of beginning of a linked list
+ * @head: address of the pointer to the first node
+ * @str: the string member of the beginning node to be created
+ *
+ * Return: the address of the new node or NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr = NULL;
+
+	ptr = malloc(sizeof(list_t));
+
+	ptr->str = (char *)str;
+	ptr->len = (int)strlen(str);
+
+	ptr->next = *head;
+	*head = ptr;
+	return (ptr);
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -2,4 +2,6 @@
 	A function that prints all the elements of a (struct list_t) list.
 1 - list_len.c
 	A function that returns the number of elements in a linked (struct list_t) list.
-2 -
+2 - add_node.c
+	A function that adds a new node at the beginning of a (struct list_t) list.
+3 -
