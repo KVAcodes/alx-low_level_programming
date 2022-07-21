58b713a6bb8173b8a2e7f2d3b8376949cb31dff3
Kadiri victor <kadiriavictor@gmail.com>
2022-07-21 20:21:22 +0000

    commit x

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,24 +2,26 @@
 
 /**
  * set_bit - sets the value of a bit at a given index
- to 1
+ * to 1
  * @n: an integer value to be operated on bitwise
  * @index: the position of the bit starting from 0
  * (left to right)
  *
  * Return: 1 if it works, or -1 of an error occurs.
  */
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int var1;
-
 	if (index >= (sizeof(unsigned long int) * 8))
 	{
 		return (-1);
 	}
-	for (var1 = 0; var1 < index; var1++)
+	*n >>= index;
+	if ((*n & 1) == 1)
 	{
-		n = n >> 1;
+		*n <<= index;
+		return (1);
 	}
-	return (n & 1);
+	*n += 1;
+	*n <<= index;
+	return (1);
 }
