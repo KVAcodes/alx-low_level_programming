8cf5622c0f2d145afff9f937637e0dd6b601a006
KVAcodes <kadiriavictor@gmail.com>
2022-09-01 19:42:20 +0000

    updated task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,65 +1,39 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: pointer to the pointer to the first node of the list.
- * @idx: index of the list where the new node should be added
- * starting from 0.
- * @n: the integer value to insert at position.
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
  *
- * Return: the address of the new node or NULL if it failed.
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *node, *new;
+	dlistint_t *tmp = *h, *new;
 
-	if (*h == NULL)
-		return (NULL);
-	node = get_dnodeint_at_index(*h, idx);
-	if (node == NULL)
-		return (NULL);
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
-	else if (!(node->next))
+
+	for (; idx != 1; idx--)
 	{
-		return (add_dnodeint_end(h, n));
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = node;
-	new->prev = node->prev;
-	node->prev->next = new;
-	node->prev = new;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
 	return (new);
-}
-
-/**
- * get_dnodeint_at_index - returns the nth node of a
- * dlistint_t linked list.
- * @head: pointer to the first node in the list.
- * @index: index of the node, starting from 0.
- *
- * Return: the address of the nth node, Otherwise
- * NULL if the node doesn't exist.
- */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{
-	unsigned int count = 0;
-	dlistint_t *ret = NULL;
-
-	if (head == NULL)
-		return (NULL);
-	while (count != index && head)
-	{
-		head = head->next;
-		count++;
-	}
-	ret = (count == index) ? head : NULL;
-
-	return (ret);
 }
