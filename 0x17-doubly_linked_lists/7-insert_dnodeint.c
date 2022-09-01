63334f4faa3218c07d67cf4a8c8e15b5bd439f50
KVAcodes <kadiriavictor@gmail.com>
2022-09-01 18:59:10 +0000

    added a 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,6 +19,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new->n = n;
+	if (*h == NULL && idx == 0)
+	{
+		new->prev = *h;
+		new->next = *h;
+		*h = new;
+		return (new);
+	}
 	if (idx == 0)
 	{
 		new->prev = NULL;
@@ -28,7 +35,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new);
 	}
 	node = get_dnodeint_at_index(*h, idx);
-	if (node->next == NULL)
+	if (node == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	else if (node->next == NULL)
 	{
 		new->prev = node;
 		new->next = NULL;
