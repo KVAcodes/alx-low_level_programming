489855bfc9b4d29a615de3a5272a161ce42de8b4
KVAcodes <kadiriavictor@gmail.com>
2022-09-02 07:44:55 +0000

    testing 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,28 +12,48 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new;
+	dlistint_t *node = get_dnodeint_at_index(*h, idx), *new;
 
+	if (node == NULL) /* index out of bounds */
+		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
-	for (; idx != 1; idx--)
-	{
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
-	}
-	if (tmp->next == NULL)
+	if (node->next == NULL)
 		return (add_dnodeint_end(h, n));
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->prev = tmp;
-	new->next = tmp->next;
-	tmp->next->prev = new;
-	tmp->next = new;
+	new->prev = node->prev;
+	new->next = node;
+	node->prev->next = new;
+	node->prev = new;
 
 	return (new);
 }
+/**
+ * get_dnodeint_at_index - returns the nth node of a
+ * dlistint_t linked list.
+ * @head: pointer to the first node in the list.
+ * @index: index of the node, starting from 0.
+ *
+ * Return: the address of the nth node, Otherwise
+ * NULL if the node doesn't exist.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *ret = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	while (count != index && head)
+	{
+		head = head->next;
+		count++;
+	}
+	ret = (count == index) ? head : NULL;
+
+	return (ret);
+}
+
