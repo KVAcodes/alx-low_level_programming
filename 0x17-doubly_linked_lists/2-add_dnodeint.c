5a18a3dc87590408f2c51f909594b81f3a3e22bc
KVAcodes <kadiriavictor@gmail.com>
2023-01-26 12:16:05 +0100

    added task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: head pointer.
+ * @n: number to add to the beginning of the list.
+ *
+ * Return: the address of the new element, or NULL if it fails.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = NULL;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return NULL;	
+	new->n = (int)n;
+	new->prev = NULL;
+	new->next = *head;
+	if (!(*head))
+	{
+		*head = new;
+		return (new);
+	}
+	(*head)->prev = new;
+	*head = new;
+
+	return (new);
+}
