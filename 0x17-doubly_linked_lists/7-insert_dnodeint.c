00265a12c5dba07d588bc449230da54a70c3e132
KVAcodes <kadiriavictor@gmail.com>
2023-01-26 14:07:56 +0100

    added task 8

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insets a new node at a given position.
+ * @h: address of the head pointer.
+ * @idx: index of insertion.
+ * @n: number to insert.
+ *
+ * Return: the address of the new node, or NULL if it fails
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int count = 0;
+	dlistint_t *ptr = *h, *new = NULL;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	if (!ptr)
+	{
+		new->prev = NULL;
+		new->next = NULL;
+		*h = new;
+		return (new);
+	}
+	while (ptr && count < idx)
+	{
+		ptr = ptr->next;
+		count++;
+	}
+	if (!ptr)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = ptr;
+	new->prev = ptr->prev;
+	ptr->prev->next = new;
+	ptr->prev = new;
+
+	return (new);
+}
