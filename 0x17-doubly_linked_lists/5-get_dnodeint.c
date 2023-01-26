9e95f08dfcbf005896ee48a609d5ec6fb840e986
KVAcodes <kadiriavictor@gmail.com>
2023-01-26 13:19:59 +0100

    added task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked
+ *			   list.
+ * @head: head pointer.
+ * @index: the index of the nth node.
+ *
+ * Return: ptr to the nth Node, NULL if the node doesn't exist.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int counter = 0;
+	dlistint_t *ptr = head;
+
+	while (ptr && counter <= index)
+	{
+		counter++;
+		ptr = ptr->next;
+	}
+	return (ptr);
+}
