4ac9832b79964a40b4d09b54b10819835173d0ca
KVAcodes <kadiriavictor@gmail.com>
2023-03-01 08:31:45 +0100

    added new

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,19 +9,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *ptr;
+	hash_node_t *ptr = NULL;
 	unsigned long int index;
 
+	if (!ht || !key)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-
 	ptr = ht->array[index];
+
 	while (ptr)
 	{
 		if (!strcmp(ptr->key, key))
-		{
 			return (ptr->value);
-		}
-
+		ptr = ptr->next;
 	}
 	return (NULL);
 }
