ea6d25b1a4494f6bf03bc129bdf6838c7874f477
KVAcodes <kadiriavictor@gmail.com>
2023-03-01 09:23:30 +0100

    added new

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: the hash table.
+ *
+ * Return: Nothing(void function)
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t **array = ht->array;
+	unsigned long int size = ht->size, count = 0;
+	hash_node_t *ptr = NULL;
+	int flag = 0;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	while (count < size)
+	{
+		ptr = array[count];
+		while (ptr)
+		{
+			if (flag)
+				printf(", ");
+			printf("\'%s\': \'%s\'", ptr->key, ptr->value);
+			flag = 1;
+			ptr = ptr->next;
+		}
+		count++;
+	}
+	printf("}\n");
+}
